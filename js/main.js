461dced568e646489c7f86198666c23c1a0f5eba
Tornike Epitashvili <itorn9777@gmail.com>
2022-06-17 15:59:00 +0400

    fixed bruger bar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ let openMenu = false;
 window.onclick = (e) => {
   const burger = document.getElementById("burger");
   const burgerContent = document.getElementById("burger-content");
+  const closeBurger = document.getElementById('close-burger');
   // open menu if menu is closed & close menu if opened
   if (!openMenu && e.target == burger) {
     burgerContent.classList.add("active");
     openMenu = true;
-  } else if (openMenu && e.target !== burgerContent) {
+  } else if (openMenu && e.target == closeBurger || e.target !== burgerContent) {
     burgerContent.classList.remove("active");
     openMenu = false;
   }
